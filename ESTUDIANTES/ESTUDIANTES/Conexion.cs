c0d20863ff2a1ed98f6164183cd31724c809ba89
BarretoCr <86670611+BarretoCr@users.noreply.github.com>
2022-03-19 11:33:25 -0500

    Guardar, actualizar, eliminar, listar y limpiar
    Programa que realiza conexion con base de datos en SQL server, se anexa toda la documentacion.

---
+++ ESTUDIANTES/ESTUDIANTES/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ESTUDIANTES
+{
+    class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("SERVER=DESKTOP-9EJ83LG\\SQLEXPRESS;DATABASE=REGISTRO;Integrated security=true;");
+            cn.Open();
+            return cn;
+
+
+        }
+    }
+}
---
+++ ESTUDIANTES/ESTUDIANTES/conexionbd.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ESTUDIANTES
+{
+    class conexionbd
+    {
+        //Cadena de Conexion
+        string cadena = "data source = DESKTOP-9EJ83LG\\SQLEXPRESS; initial catalog = REGISTRO; Integrated Security=True";
+
+        public SqlConnection Conectarbd = new SqlConnection();
+
+        //Constructor
+        public conexionbd()
+        {
+            Conectarbd.ConnectionString = cadena;
+        }
+
+        //Metodo para abrir la conexion
+        public void abrir()
+        {
+            try
+            {
+                Conectarbd.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error al abrir BD "+ex.Message);
+            }
+        }
+
+        //Metodo para cerrar la conexion
+        public void cerrar()
+        {
+            Conectarbd.Close();
+        }
+    }
+}
